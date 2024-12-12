1dcc125487a20817066a3c0437376e1ebc3412d0
DatOris <ntdathttt2211003@student.ctuet.edu.vn>
2024-12-13 00:22:50 +0700

    dash board big update bruh
    met muon chet cái lùm mía

---
+++ ViewModels/DashboardViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using ThienAnFuni.Models;
+
+public class DashboardViewModel
+{
+    public int TotalCustomers { get; set; }
+    public int TotalProducts { get; set; }
+    public int TotalOrders { get; set; }
+    public int LowStockProducts { get; set; }
+    public List<OrderViewModel> RecentOrders { get; set; }
+    public List<Customer> NewCustomers { get; set; }
+    public List<User> CustomerUsers { get; set; }
+}
+
+public class OrderViewModel
+{
+    public int Id { get; set; }
+    public string CustomerName { get; set; }
+    public double TotalAmount { get; set; }
+    public int? Status { get; set; }
+}
